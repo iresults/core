88f6f8a17b0f5d4b77bfe16a10d6d7f58f73345b
Daniel Corn <d@cundd.net>
2016-10-05 15:45:20 +0200

    Removed the alias for KVC_Interface

--- Classes/Iresults/Core/KVCInterface.php
+++ Classes/Iresults/Core/KVCInterface.php
@@ -1,73 +1,56 @@
 <?php
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Andreas Thurnheer-Meier <tma@iresults.li>, iresults
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace Iresults\Core;
 
-    /*
-     * The MIT License (MIT)
-     * Copyright (c) 2013 Andreas Thurnheer-Meier <tma@iresults.li>, iresults
-     *
-     * Permission is hereby granted, free of charge, to any person obtaining a copy
-     * of this software and associated documentation files (the "Software"), to deal
-     * in the Software without restriction, including without limitation the rights
-     * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     * copies of the Software, and to permit persons to whom the Software is
-     * furnished to do so, subject to the following conditions:
-     *
-     * The above copyright notice and this permission notice shall be included in
-     * all copies or substantial portions of the Software.
-     *
-     * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     * SOFTWARE.
-     */
-
 /**
- * Abstract class for the cache classes. Defines a common interface.
- *
- * @author        Daniel Corn <cod@iresults.li>
- * @package       Iresults
- * @subpackage    Iresults_Core
+ * Interface for classes that provide key based access to properties
  */
 interface KVCInterface
 {
     /**
-     * Returns the object at the given key.
+     * Returns the value at the given key
      *
-     * @param    string $key
-     * @return    mixed
+     * @param string $key
+     * @return mixed
      */
     public function getObjectForKey($key);
 
     /**
-     * Stores the value of $object at the given key.
+     * Stores the value of $object at the given key
      *
-     * @param    string $key
-     * @param    mixed  $object
-     * @return    void
+     * @param string $key
+     * @param mixed  $object
+     * @return void
      */
     public function setObjectForKey($key, $object);
 
     /**
      * Removes the object with the given key.
      *
-     * @param    string $key
-     * @return    void
+     * @param string $key
+     * @return void
      */
     public function removeObjectForKey($key);
-}
-
-/*
- * Create a alias of \Iresults\Core\KVCInterface with the name KVC_Interface.
- */
-if (interface_exists('KVC_Interface', false)) {
-    trigger_error(
-        'The interface \'KVC_Interface\' already exists.<br />
-				  Will not create an alias for \Iresults\Core\KVCInterface.',
-        E_USER_WARNING
-    );
-} else {
-    class_alias('\Iresults\Core\KVCInterface', '\KVC_Interface');
 }
